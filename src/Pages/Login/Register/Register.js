1be60cf19d3667a7ec531e303db72b3c05da32e6
roxy1992 <roxybhuiyan1992@gmail.com>
2022-04-17 23:47:46 +0600

    add link in nav

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.email.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+    }
+
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='your mail' />
+
+                <input type="password" name="password" id="" placeholder='your password' />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
         </div>
     );
 };
